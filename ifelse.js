2c60cb4527776fc79127ac16398a3c8562a46968
SAH <143020123+BRINAHH@users.noreply.github.com>
2024-02-28 10:55:11 -0300

    Update ifelse.js

--- ifelse.js
+++ ifelse.js
@@ -4,10 +4,10 @@
 var x = 1;//a variavel X e igual a 1
 var y = 1;// a variavel Y e igual a 1
 
-//Estrutura de possibiliodade de x e y tem o mesmo valor 
+//Estrutura de possibilidade de x e y tem o mesmo valor 
 if (x == y) {
     console.log("Número conferem!");//console.log coloca tudo no console numero conferem
-
-} else {// significa SENAO, isso apos o IF
+//Na linha abaixo esta acontecendo uma verificacao, se por exemplo na linha do IF foi falsa tera uma outra alternativa que sera o ELSE 
+} else {
     console.log("Número diferem!");// aparece no console numero diferem
 }
